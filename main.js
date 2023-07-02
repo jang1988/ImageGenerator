fb8f36d1e8a830353a7b44ad064984a89f29197f
Baklay Petro <pustota1988@gmail.com>
2023-07-02 18:56:39 +0300

    deployVersion

---
+++ main.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import config from 'config'
+import { engine } from 'express-handlebars'
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: config.get('OPENAI_KEY'),
+})
+const openai = new OpenAIApi(configuration)
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_, res) => {
+  res.render('index')
+})
+
+app.post('/', async (req, res) => {
+  const prompt = req.body.prompt
+  const size = req.body.size ?? '512x512'
+  const number = req.body.number ?? 1
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      size,
+      n: Number(number),
+    })
+
+    res.render('index', {
+        images: response.data.data,
+      })
+  } catch (e) {
+    console.error('Error:', e.response.data.error.message);
+    res.render('index', {
+      error: e.message,
+    })
+  }
+})
+
+app.listen(80, () => console.log('Server started...'))
